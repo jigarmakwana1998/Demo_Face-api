4d053fa63ffd98148943798d0cdd3abd59ee8085
jigarmakwana1998 <makwanajigar17@gmail.com>
2020-05-27 17:40:57 +0530

    working till 5th Kernel

--- commons.js
+++ commons.js
@@ -14,22 +14,17 @@ function startVideo() {
   )
 }
 
-var width = 256, height = 256, buffer = new Uint8ClampedArray(width * height * 4);
-
-
-var canvas1 = document.getElementById('canvas'),
-  ctx = canvas1.getContext('2d');
-
+var width = 256, height = 256;
+var canvas1 = document.getElementById('canvas'), ctx = canvas1.getContext('2d');
 canvas1.width = width;
 canvas1.height = height;
-
+var idata = ctx.createImageData(width, height);
 const container = document.createElement('div')
 container.style.position = 'relative'
 document.body.append(container)
-
 container.append(video)
-video.addEventListener('play', () => {
 
+video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video);
   container.append(canvas);
 
@@ -38,9 +33,8 @@ video.addEventListener('play', () => {
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.SsdMobilenetv1Options());
     // image2 = await faceapi.nets.ssdMobilenetv1.getConvLayer();
-    grayScale = await faceapi.nets.ssdMobilenetv1.getGrayScale();
-    // console.log(image2);
-    var idata = ctx.createImageData(width, height);
+    const kernelNumber = document.getElementById("frm1").elements[0].value;
+    grayScale = await faceapi.nets.ssdMobilenetv1.getGrayScale(kernelNumber);
     idata.data.set(grayScale);
     ctx.putImageData(idata, 0, 0);
 
