0a26860ba57ac200c97f41dbccf4aabcf43b5038
jigarmakwana1998 <makwanajigar17@gmail.com>
2020-06-05 21:38:55 +0530

    Regular Update

--- commons.js
+++ commons.js
@@ -7,7 +7,7 @@ Promise.all([
 ]).then(startVideo)
 
 async function startVideo() {
-  const stream = await navigator.mediaDevices.getUserMedia({ video: true})
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true })
   video.srcObject = stream
 }
 
@@ -20,6 +20,7 @@ const container = document.createElement('div')
 container.style.position = 'relative'
 document.body.append(container)
 container.append(video)
+kernel = 0
 
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video);
@@ -30,14 +31,22 @@ video.addEventListener('play', () => {
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.SsdMobilenetv1Options());
 
-    const kernelNumber = document.getElementById("frm1").elements[0].value;
-    grayScale = await faceapi.nets.ssdMobilenetv1.getGrayScale(kernelNumber);
-    idata.data.set(grayScale);
-    ctx.putImageData(idata, 0, 0);
+    // const kernelNumber = document.getElementById("frm1").elements[0].value;
+
+    // setInterval(async () => {
+      const layer = await faceapi.nets.ssdMobilenetv1.getConvLayer();
+      layer_kernel = layer.slice(kernel, kernel + 1)[0];
+      document.getElementById("kernel").innerHTML = kernel;
+
+      grayScale = await faceapi.nets.ssdMobilenetv1.getGrayScale(layer_kernel);
+      idata.data.set(grayScale);
+      ctx.putImageData(idata, 0, 0);
+      kernel = (kernel + 1) % 64;
+    // }, 500)
 
     const resizedDetections = faceapi.resizeResults(detections, displaySize);
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
     faceapi.draw.drawDetections(canvas, resizedDetections);
 
-  }, 200)
+  }, 500)
 })
--- examples-browser/public/js/faceDetectionControls.js
+++ examples-browser/public/js/faceDetectionControls.js
@@ -1,13 +1,30 @@
 // let selectedFaceDetector = SSD_MOBILENETV1
 let minConfidence = 0.5
+let flag = 0
 
 let kernelNumber = 0
 function getFaceDetectorOptions() {
   return  new faceapi.SsdMobilenetv1Options({ minConfidence })
 }
 
+function auto_update(){
+  update = setInterval(onIncreaseMinConfidence,200)
+  if (flag > 0){
+    flag = 0
+    kernelNumber = 0
+    $('#kernelNumber').val(kernelNumber)
+  }
+  
+  if(flag < 1 && $('#kernelNumber').val() > 62){
+    flag = 1
+  }
+  console.log(flag)
+  updateResults()
+}
+
 function onIncreaseMinConfidence() {
-  kernelNumber = Math.min(faceapi.utils.round(kernelNumber + 1), 63)
+  kernelNumber = (kernelNumber + 1)% 64
+  // kernelNumber = Math.min(faceapi.utils.round(kernelNumber + 1), 63)
   $('#kernelNumber').val(kernelNumber)
   updateResults()
 }
